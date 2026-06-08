ea57d27e018f1d1b939f4d36388a0bacda405c7f
SiddhantKumar05 <siddhantkumarprayagraj@gmail.com>
2026-06-08 15:55:30 +0530

    Day 8 Assignment Uploaded

---
+++ Day_8/Q32.java
@@ -0,0 +1,27 @@
+package Day_8;
+
+import java.util.*;
+
+public class Q32 {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        if(n <= 0)
+        {
+            System.out.println("Invalid Input");
+            return;
+        }
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
