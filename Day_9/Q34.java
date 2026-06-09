085fae15f88146eab27417e4b47f76abf0353ff8
SiddhantKumar05 <siddhantkumarprayagraj@gmail.com>
2026-06-09 11:15:42 +0530

    Day 9 Assignment Uploaded

---
+++ Day_9/Q34.java
@@ -0,0 +1,27 @@
+package Day_9;
+
+import java.util.*;
+
+public class Q34 {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        if(n <= 0)
+        {
+            System.out.println("Invalid Input");
+            return;
+        }
+
+        for(int i = n; i > 0; i--)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day_9/Q36.java
@@ -0,0 +1,52 @@
+package Day_9;
+
+import java.util.*;
+
+public class Q36 {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        if(n <= 0)
+        {
+            System.out.println("Invalid Input");
+            return;
+        }
+
+        if(n == 1)
+        {
+            System.out.println("*");
+            return;
+        }
+        
+        for(int i = 1; i <= n; i++)
+        {
+            System.out.print("*");
+        }
+
+        System.out.println();
+
+        for(int i = 1; i <= n-2; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(j == 1 || j == n)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i <= n; i++)
+        {
+            System.out.print("*");
+        }
+    }
+}
