3e6666af4b576e9cb5cfdaaa2474f555e08a0779
SiddhantKumar05 <siddhantkumarprayagraj@gmail.com>
2026-06-10 15:19:45 +0530

    Day 1 Assignment Uploaded

--- Day_01/Q2.java
+++ Day_01/Q2.java
@@ -1,17 +1,15 @@
-package Day_1;
+package Day_01;
 
 import java.util.Scanner;
 
-public class Q1 {
+public class Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number: "); 
         int n = sc.nextInt();
-        int sum = 0;
-        for(int i = 1; i <= n; i++)
+        for(int i = 1; i <= 10; i++)
         {
-            sum = sum + i;
+            System.out.println(n + " X " + i + " = " + n*i); 
         }
-        System.out.println("The sum of first N natural numbers is " + sum); 
-    }    
+    }
 }
